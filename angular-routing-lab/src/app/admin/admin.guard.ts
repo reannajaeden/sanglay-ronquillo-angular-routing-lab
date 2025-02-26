c58fa601d4866032382fdc5317737dc7da28e3ef
reannajaeden <20236039@s.ubaguio.edu>
2025-02-26 12:35:42 +0800

    implemented guard logic

--- angular-routing-lab/src/app/admin/admin.guard.ts
+++ angular-routing-lab/src/app/admin/admin.guard.ts
@@ -1,5 +1,15 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
 
-export const adminGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate {
+  canActivate(
+    _next: ActivatedRouteSnapshot,
+    _state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true; 
+  }
+}
--- angular-routing-lab/src/app/app.routes.ts
+++ angular-routing-lab/src/app/app.routes.ts
@@ -4,18 +4,20 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { ProfileComponent } from './about/profile/profile.component';
+import { AdminGuard } from './admin/admin.guard';
 
 
 
-export const routes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent },
-    { path: 'about', component: AboutComponent, children: [
-      { path: 'profile', component: ProfileComponent }
-    ]},
-    { path: 'contact', component: ContactComponent },
-    { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }
-  ];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent, children: [
+    { component: ProfileComponent }
+  ]},
+  { path: 'contact', component: ContactComponent },
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule), canActivate: [AdminGuard] }
+];
+
  
 
 @NgModule({
