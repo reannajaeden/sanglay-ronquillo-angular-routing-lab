14864b152a941980567857d7a6e7e690ecd269aa
Chrizendrex <20180054@s.ubaguio.edu>
2025-02-26 11:47:58 +0800

    Updated imports in app components ts

--- angular-routing-lab/src/app/app.component.ts
+++ angular-routing-lab/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AboutComponent,ContactComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
