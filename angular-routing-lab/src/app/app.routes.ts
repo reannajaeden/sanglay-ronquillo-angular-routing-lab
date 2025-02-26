04efd5e020afca8d96247447a2ff34705e68ac27
Chrizendrex <20180054@s.ubaguio.edu>
2025-02-26 12:05:48 +0800

    Updated the approutes for child route

--- angular-routing-lab/src/app/app.routes.ts
+++ angular-routing-lab/src/app/app.routes.ts
@@ -3,14 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { ProfileComponent } from './about/profile/profile.component';
+
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'contact', component: ContactComponent }];
-
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent, children: [
+      { path: 'profile', component: ProfileComponent }
+    ]},
+    { path: 'contact', component: ContactComponent }
+  ];
+ 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
