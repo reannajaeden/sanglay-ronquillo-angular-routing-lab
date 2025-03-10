7a0576d279ab18e0f9104ec30bdb18f92b04d094
Chrizendrex <20180054@s.ubaguio.edu>
2025-03-10 11:33:28 +0800

    Fixed every components to make the router link or outlet to work
    We made many changes to make the router outlet to work in order to see our created html files

--- angular-routing-lab/src/app/app.routes.ts
+++ angular-routing-lab/src/app/app.routes.ts
@@ -1,12 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './about/profile/profile.component';
-
+import { AdminGuard } from './admin/admin.guard';
 
 export const routes: Routes = [
-    {path: 'about', component: AboutComponent, children: [{path: 'profile', component: ProfileComponent}]},
-    {path: 'contact', component: ContactComponent},
-    {path: 'home', component: HomeComponent}
-];
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent, children: [
+        { path: 'profile', component: ProfileComponent }
+      ] 
+    },
+    { path: 'contact', component: ContactComponent },
+    { 
+      path: 'admin', 
+      loadComponent: () => import('./admin/admin.component').then(m => m.AdminComponent),
+      canActivate: [AdminGuard] // Apply guard
+    }
+  ];
+  
+  
+  
