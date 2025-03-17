507fb98f80fc6b9fd71a219105b42aab218939d8
reannajaeden <20236039@s.ubaguio.edu>
2025-03-17 12:01:39 +0800

    created two child components

---
+++ angular-routing-lab/src/app/child-one/child-one.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-child-one',
+  imports: [],
+  templateUrl: './child-one.component.html',
+  styleUrl: './child-one.component.css'
+})
+export class ChildOneComponent {
+
+}
---
+++ angular-routing-lab/src/app/child-two/child-two.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-child-two',
+  imports: [],
+  templateUrl: './child-two.component.html',
+  styleUrl: './child-two.component.css'
+})
+export class ChildTwoComponent {
+
+}
