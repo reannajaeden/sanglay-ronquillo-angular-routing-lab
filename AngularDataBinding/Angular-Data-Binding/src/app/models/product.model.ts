48c31c4a659a7ec7277a9f7a0b877f0a8b4bf18c
reannajaeden <20236039@s.ubaguio.edu>
2025-03-26 11:57:03 +0800

    Create product.model.ts

---
+++ AngularDataBinding/Angular-Data-Binding/src/app/models/product.model.ts
@@ -0,0 +1,11 @@
+export interface Product {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    imageUrl: string;
+    isAvailable: boolean;
+    category: string;
+    rating: number;
+    quantity: number;
+    }
